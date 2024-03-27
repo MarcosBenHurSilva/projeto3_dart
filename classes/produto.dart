cb7962aba021b3fd6829e5305c59a81472a80d85
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2024-03-27 19:55:46 -0300

    style: :art: corrigi o método
    correção no bloco if-else do método realizarVenda
    
    mudança de estilo no código

--- classes/produto.dart
+++ classes/produto.dart
@@ -19,18 +19,15 @@ class Produto {
   void realizarVenda() {
     if (_qtdEmEstoque < 0) {
       throw ArgumentError('Quantidade inválida.');
-    }
-
-    if (_qtdEmEstoque == 0) {
+    } else if (_qtdEmEstoque == 0) {
       print('No momento não possuímos o produto $nome em estoque.');
-      return;
+    } else {
+      _qtdEmEstoque -= 1;
+      _qtdVendida += 1;
+
+      print('Compra de um(a) produto $nome realizada com sucesso!');
+      print('Quantidade em estoque: $_qtdEmEstoque');
     }
-
-    _qtdEmEstoque -= 1;
-    _qtdVendida += 1;
-
-    print('Compra de um(a) produto $nome realizada com sucesso!');
-    print('Quantidade em estoque: $_qtdEmEstoque');
   }
 
   // Getters para os atributos privados
